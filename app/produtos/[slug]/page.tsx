51e461eca86edacdb7948549339ee91fa515a617
sayberunlock <gheoliveira86@gmail.com>
2026-09-25 02:26:15 -0300

    ATUALIZAÇÃO 1.0.1

--- app/produtos/[slug]/page.tsx
+++ app/produtos/[slug]/page.tsx
@@ -71,7 +71,7 @@ export default async function ProductDetailPage({ params }: Props) {
             alt={product.name}
             fill
             sizes="(max-width: 1024px) 100vw, 50vw"
-            className="object-contain p-10"
+            className="object-contain p-4"
             priority
           />
         </div>
